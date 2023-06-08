3b95d08a4a90d41fbaa927403f7b9bb10cc23242
Gowrisankar <102644027+gowrisankardoodleblue@users.noreply.github.com>
2023-06-08 10:41:33 +0530

    Update router.js

--- app/router/router.js
+++ app/router/router.js
@@ -78,6 +78,23 @@ const {
 } = require("../controllers/contentManagement.controller");
 
 const {
+  contentList,
+  createContent,
+  getContent,
+  updateContent,
+  deleteContent,
+} = require("../controllers/categoryManagement.controller");
+
+
+const {
+  contentList,
+  createContent,
+  getContent,
+  updateContent,
+  deleteContent,
+} = require("../controllers/subCategoryManagement.controller");
+
+const {
   createSiteSettings,
   getSiteSettings,
   updateSiteSettings,
@@ -386,60 +403,60 @@ router.delete(
 
 
 
-//category Management
-router.get(
-  routes.v1.categoryManagement.list,
-  [verifyToken(["ADMIN","AP"]), categoryListValidation],
-  errHandle(categoryList)
-);
-router.post(
-  routes.v1.categoryManagement.create,
-  [verifyToken(["ADMIN"]), createCategoryValidation],
-  errHandle(createCategory)
-);
-router.get(
-  routes.v1.categoryManagement.get,
-  [verifyToken(["ADMIN","AP"]), getCategoryValidation],
-  errHandle(getCategory)
-);
-router.put(
-  routes.v1.categoryManagement.update,
-  [verifyToken(["ADMIN"]), updateCategoryValidation],
-  errHandle(updateCategory)
-);
-router.delete(
-  routes.v1.categoryManagement.delete,
-  [verifyToken(["ADMIN"]), deleteCategoryValidation],
-  errHandle(deleteCategory)
-);
+// //category Management
+// router.get(
+//   routes.v1.categoryManagement.list,
+//   [verifyToken(["ADMIN","AP"]), categoryListValidation],
+//   errHandle(categoryList)
+// );
+// router.post(
+//   routes.v1.categoryManagement.create,
+//   [verifyToken(["ADMIN"]), createCategoryValidation],
+//   errHandle(createCategory)
+// );
+// router.get(
+//   routes.v1.categoryManagement.get,
+//   [verifyToken(["ADMIN","AP"]), getCategoryValidation],
+//   errHandle(getCategory)
+// );
+// router.put(
+//   routes.v1.categoryManagement.update,
+//   [verifyToken(["ADMIN"]), updateCategoryValidation],
+//   errHandle(updateCategory)
+// );
+// router.delete(
+//   routes.v1.categoryManagement.delete,
+//   [verifyToken(["ADMIN"]), deleteCategoryValidation],
+//   errHandle(deleteCategory)
+// );
 
 
-//subcategory Management
-router.get(
-  routes.v1.subCategoryManagement.list,
-  [verifyToken(["ADMIN","AP"]), subCategoryListValidation],
-  errHandle(subCategoryList)
-);
-router.post(
-  routes.v1.subCategoryManagement.create,
-  [verifyToken(["ADMIN"]), createSubCategoryValidation],
-  errHandle(createSubCategory)
-);
-router.get(
-  routes.v1.subCategoryManagement.get,
-  [verifyToken(["ADMIN","AP"]), getSubCategoryValidation],
-  errHandle(getSubCategory)
-);
-router.put(
-  routes.v1.categoryManagement.update,
-  [verifyToken(["ADMIN"]), updateSubCategoryValidation],
-  errHandle(updateSubCategory)
-);
-router.delete(
-  routes.v1.subCategoryManagement.delete,
-  [verifyToken(["ADMIN"]), deleteSubCategoryValidation],
-  errHandle(deleteSubCategory)
-);
+// //subcategory Management
+// router.get(
+//   routes.v1.subCategoryManagement.list,
+//   [verifyToken(["ADMIN","AP"]), subCategoryListValidation],
+//   errHandle(subCategoryList)
+// );
+// router.post(
+//   routes.v1.subCategoryManagement.create,
+//   [verifyToken(["ADMIN"]), createSubCategoryValidation],
+//   errHandle(createSubCategory)
+// );
+// router.get(
+//   routes.v1.subCategoryManagement.get,
+//   [verifyToken(["ADMIN","AP"]), getSubCategoryValidation],
+//   errHandle(getSubCategory)
+// );
+// router.put(
+//   routes.v1.categoryManagement.update,
+//   [verifyToken(["ADMIN"]), updateSubCategoryValidation],
+//   errHandle(updateSubCategory)
+// );
+// router.delete(
+//   routes.v1.subCategoryManagement.delete,
+//   [verifyToken(["ADMIN"]), deleteSubCategoryValidation],
+//   errHandle(deleteSubCategory)
+// );
 
 
 module.exports = router;
