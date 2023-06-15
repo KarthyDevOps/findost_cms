b0f8fa4c304d4be9001acf3ac4d2466216cbe884
kishoreDevolper <kishore.t@doodleblue.in>
2023-06-15 11:16:02 +0530

    code update

--- app/models/feedback.js
+++ app/models/feedback.js
@@ -27,7 +27,8 @@ const feedbackSchema = new mongoose.Schema(
     },
    
     status: {     
-      type: String, 
+      type: String,
+      default:"Open"
     },
     isDeleted: {
       type: Boolean,
